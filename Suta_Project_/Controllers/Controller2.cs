efdcb3164f6fd3077656abb245d94ede9119a458
PraveenRanguu <prangu@innominds.com>
2021-11-23 18:56:57 +0530

    Add project files.

---
+++ Suta_Project_/Controllers/Controller2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Suta_Model;
+using Suta_Project;
+using Suta_Repo;
+
+namespace Suta_Project.Controllers
+{
+   //[Route("api/[controller]")]
+   // [ApiController]
+    public class Controller2 : ControllerBase
+    {
+       
+        
+            private readonly Iservices2 _productService;
+            public Controller2(Iservices2 studentService)
+            {
+            _productService = studentService;
+            }
+
+            [HttpGet, Route("api/Controller2/Getproduct")]
+            public IEnumerable<productdetails> Getproduct()
+            {
+                return _productService.Getproductdetails();
+            }
+        }
+    }
+
+
---
+++ Suta_Project_/Controllers/SutaController1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Suta_ConnectionString;
+using Suta_Model;
+using Suta_Project;
+using Suta_Repo;
+
+
+namespace Suta_Project.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class SutaController1 : ControllerBase
+    {
+        private readonly IsutaServices _sutaService;
+        public SutaController1(IsutaServices sutaaService)
+        {
+            _sutaService = sutaaService;
+        }
+
+
+        [HttpGet, Route("api/Users")]
+        public IEnumerable<Customerdetails> Getusers()
+        {
+            return _sutaService.GetCustomerdetails();
+        }
+
+
+        [HttpPost, Route("api/InsertUser/{Name}/{Password}/{Email}/{Phnno}/{Gender}")]
+        public IEnumerable<Customerdetails> InsertUser(Customerdetails Student1)
+        {
+            if (_sutaService.InsertCustomer(Student1))
+            {
+                return _sutaService.GetCustomerdetails();
+            }
+
+            return Enumerable.Empty<Customerdetails>();
+        }
+
+        [HttpPost, Route("api/Login")]
+        public bool Suta_Login(string Email, string Password1)
+        {
+            if (_sutaService.user_Login(Email, Password1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        [HttpGet, Route("api/ForgotPassword")]
+        public bool ForgotPWd(string Pwd, string email)
+        {
+            if (_sutaService.ForgotPassword(Pwd, email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [HttpPost, Route("api/DeleteUser")]
+        public bool delete(string Name)
+        {
+            if (_sutaService.DeleteUser(Name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+
+        }
+
+
+    }
+}
